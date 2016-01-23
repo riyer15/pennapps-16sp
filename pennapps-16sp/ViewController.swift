d2583e50e43a1916c329d8860d824e1e6ca74c0c
Veronica Wharton <wharton.veronica@gmail.com>
2016-01-22 23:18:20 -0500

    Initial commit

---
+++ pennapps-16sp/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  pennapps-16sp
+//
+//  Created by Veronica Wharton on 1/22/16.
+//  Copyright © 2016 hack4impact. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let camera = GMSCameraPosition.cameraWithLatitude(-33.86,
+            longitude: 151.20, zoom: 6)
+        let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+        mapView.myLocationEnabled = true
+        self.view = mapView
+        
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ pennapps-16spTests/pennapps_16spTests.swift
@@ -0,0 +1,36 @@
+//
+//  pennapps_16spTests.swift
+//  pennapps-16spTests
+//
+//  Created by Veronica Wharton on 1/22/16.
+//  Copyright © 2016 hack4impact. All rights reserved.
+//
+
+import XCTest
+@testable import pennapps_16sp
+
+class pennapps_16spTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
